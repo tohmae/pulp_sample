f77fbe2db955bed23cc054e9a3dd644ecf3037f2
tohmae <t.ohmae.nx01@gmail.com>
2020-02-12 22:43:32 +0900

    commit

--- score_optimize.py
+++ score_optimize.py
@@ -17,9 +17,10 @@ np.random.seed(10)
 scores = np.random.rand(USER, ITEM)
 scores[:,0] += 0.3
 scores[:,1] += 0.3
-scores[:,18] -= 0.1
+scores[:,18] -= 0.3
 scores[:,19] -= 0.3
 scores = np.clip(scores, 0, 1)
+print(scores)
 
 prob = pulp.LpProblem("test",pulp.LpMaximize)
 
@@ -31,22 +32,21 @@ choices = pulp.LpVariable.dicts("Choice",(Users,Items) , 0, 1, pulp.LpInteger)
 prob += pulp.lpSum([scores[u][i] * choices[u][i] for u in Users for i in Items ])
 
 # 制約条件
-#1. $\sum_{i} choice_{ui} <= 5  (\forall u)$
+#1. $\sum_{i} choice_{ui} <= 5 (\forall u)$
 for u in Users:
     prob += pulp.lpSum([choices[u][i] for i in Items]) <= 5
 
-#2. $\sum_{u} choice_{ui} <= 10  (\forall i) $
-for i in Items:
-    prob += pulp.lpSum([choices[u][i] for u in Users]) <= 10
-
 if args.restrict == 1:
-    #3. $\sum_{u} choice_{ui} <= 3  (i=0,1) $
+    #2. $\sum_{u} choice_{ui} <= 10 (\forall i) $
+    for i in Items:
+        prob += pulp.lpSum([choices[u][i] for u in Users]) <= 10
+    #3. $\sum_{u} choice_{ui} <= 3 (i=0,1) $
     for i in [0,1]:
         prob += pulp.lpSum([choices[u][i] for u in Users]) <= 3
 
-    #4. $\sum_{u} choice_{ui} >= 7  (i=18,19) $
+    #4. $\sum_{u} choice_{ui} >= 9 (i=18,19) $
     for i in [18,19]:
-        prob += pulp.lpSum([choices[u][i] for u in Users]) >= 7
+        prob += pulp.lpSum([choices[u][i] for u in Users]) >= 9
 
 status = prob.solve()
 print("Status:", pulp.LpStatus[status])
